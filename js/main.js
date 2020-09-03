bd0538c1961f0bce230511475634667ecf9e7c9c
h2312 <np71379@gmail.com>
2020-09-03 22:00:01 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    if (!validateForm(siteName, siteUrl)) {
+        return false;
+    }
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    // Local Storage only store string
+    if (localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    fetchBookmarks()
+    e.preventDefault();
+}
+
+function deleteBookmark(url) {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
+            bookmarks.splice(i, 1);
+        }
+    }
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks()
+}
+
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResult = document.getElementById('bookmarksResults');
+    bookmarksResult.innerHTML = '';
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+        bookmarksResult.innerHTML += '<div class="card bg-light text-dark card-body">' + name +
+            ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">x</a> ' +
+            '</div>';
+    }
+}
+
+function validateForm(siteName, siteUrl) {
+    if (!siteName || !siteUrl) {
+        alert('Please fill in the form');
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Please use a valid URL');
+        var siteUrl = document.getElementById('siteUrl');
+        siteUrl.focus();
+        return false;
+    }
+
+    return true;
+}
